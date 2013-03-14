86b4562b5f80bfd5e5fca281e4d44203e2d94a24
Jamie Davidson <jhdavids8@gmail.com>
2013-03-14 14:42:59 -0400

    Initial commit

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,4 @@
+//= require jquery_ujs
+//= require jquery-ui
+//= require foundation
+$(document).foundation();
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SIBJoustPartnerProject::Application.config.secret_token = '6be94b3c7f6ab6cfba7e8f27056e63a1cb2f3d6e7e2ce317698c78779477f55d8048f047a8dea362d513de635a5d8dfbe11851d6a411258329a70c2185969ba0'
