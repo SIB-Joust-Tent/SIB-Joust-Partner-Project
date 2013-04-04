48f722176f6df221040a1ae6f17630da72749493
skscharr <skscharr@gmail.com>
2013-04-04 17:49:22 -0400

    create migration for displayed trello board options, create code for enumeration of board data for admin page

--- app/controllers/trello/api_controller.rb
+++ app/controllers/trello/api_controller.rb
@@ -11,7 +11,7 @@ class Trello::ApiController < ApplicationController
 		lists = "&lists=open&list_fields=name"
 		cards = "&cards=visible&card_fields=idList,name,desc&card_attachments=true&card_attachment_fields=name,url"
 
-		uri = URI.parse("https://api.trello.com/1/boards/511c01ca8f642cbd1c0053b5?#{lists}#{cards}&key=#{public_key}&token=#{token}")
+		uri = URI.parse("https://api.trello.com/1/boards/511c01ca8f642cbd1c0053b5?fields=closed&#{lists}#{cards}&key=#{public_key}&token=#{token}")
    
    	respond_with(JSON.parse uri.open.read)
 	end
--- app/models/company.rb
+++ app/models/company.rb
@@ -1,9 +1,28 @@
+require 'open-uri'
+require 'json'
+
 class Company < ActiveRecord::Base
   extend FriendlyId
   friendly_id :name, use: :slugged
   
   attr_accessible :description, :facebookhandle, :image, :location, :name, :rssfeed, 
   				  			:tagline, :twitterhandle, :url
+
+  serialize :displayed_trello_boards, Array
+
+  def displayed_trello_boards_enum
+  	public_key = ENV['TRELLO_PUBLIC_KEY']
+		token = ENV['TRELLO_TOKEN']
+
+		uri =  JSON.parse(URI.parse("https://api.trello.com/1/boards/511c01ca8f642cbd1c0053b5?fields=closed&lists=open&list_fields=name&key=#{public_key}&token=#{token}").open.read)
+		lists = uri.assoc("lists")
+		lists[1].map{|l| [l["name"], l["id"]] }
+  end
+
+  def has_displayed_trello_boards?(board)
+  	available_boards = self.boards.map{ |r| self.displayed_trello_boards_enum(r)}
+  	available_boards.include?(board)
+  end
 
   has_many :users
 end
