e72898d9e4158d0131eaf5a398ab5a3b253166a3
Jamie Davidson <jhdavids8@gmail.com>
2013-04-16 10:02:31 -0400

    Minor

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,7 @@
 # It's strongly recommended to check this file into your version control system.
 
 ActiveRecord::Schema.define(:version => 20130415034745) do
+
   create_table "companies", :force => true do |t|
     t.string   "name"
     t.string   "tagline"
